00fd172af1bfefcc849fbbbc81c853344b6aa278
Lalo <lalo.sa.94@gmail.com>
2015-03-19 21:10:00 -0600

    Initial Commit

---
+++ QuimiPop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuimiPop
+//
+//  Created by Eduardo Alberto Sanchez Alvarado on 3/19/15.
+//  Copyright (c) 2015 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
